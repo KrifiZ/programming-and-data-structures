438a664592c3706a948ce1a2bdf1b513eb2e9007
Michal Muller <michalmuller@onet.eu>
2026-04-22 20:26:54 +0200

    feat: add Tetris game implementation with graphics and game logic

---
+++ tetris/pieces.h
@@ -0,0 +1,10 @@
+#ifndef PIECES_H
+#define PIECES_H
+
+#define PIECE_COUNT 7
+#define NUM_ROTATIONS 4
+#define PIECE_SIZE 4
+
+extern char pieces[PIECE_COUNT][NUM_ROTATIONS][PIECE_SIZE][PIECE_SIZE];
+
+#endif
